a2bf6a5d656ebe6b982aa8f8c3837afaea9e6ca3
islombekt <islombek_t@bk.ru>
2024-10-13 00:26:10 +0500

    Add project files.

---
+++ SynelTest/Entities/Employees.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SynelTest.Entities
+{
+    public class Employees
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string PayrollNumber { get; set; }
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Telephone { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Address_2 { get; set; }
+        public string Postcode { get; set; }
+        public string EMail_Home { get; set; }
+        public DateOnly Start_Date { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ SynelTest/Models/PersonalRecords.cs
@@ -0,0 +1,17 @@
+﻿namespace SynelTest.Models
+{
+    public class PersonalRecords
+    {
+        public string PayrollNumber { get; set; }
+        public string Forenames { get; set; }
+        public string Surname { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Telephone { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string Address_2 { get; set; }
+        public string Postcode { get; set; }
+        public string EMail_Home { get; set; }
+        public DateOnly Start_Date { get; set; }
+    }
+}
